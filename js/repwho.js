0b78fadb2fb70a3efe670675db4a6643a9805551
Soumya Deb <debloper@gmail.com>
2013-08-30 22:35:46 +0530

    Return one Rep from the list, randomly

--- js/repwho.js
+++ js/repwho.js
@@ -1,8 +1,14 @@
 $(document).ready(
-
-$.getJSON('https://reps.mozilla.org/api/v1/rep/?offset=0&limit=0&group=council&callback=?', 
-    function(data) {
-        console.log('got it!');
-        $('body').append(JSON.stringify(data));
-    })
+	$.getJSON('https://reps.mozilla.org/api/v1/rep/?offset=0&limit=0&group=council&callback=?',
+		function (data) {
+			$('body').append(
+				JSON.stringify(
+					data.objects[Math.floor(Math.random() * data.objects.length)]
+				)
+			);
+		},
+		function (error) {
+			console.log(error);
+		}
+	)
 );
