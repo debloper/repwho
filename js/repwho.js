c4e0d747d768b3d0ffe62b0d6dd4f32ecda3ce69
Soumya Deb <debloper@gmail.com>
2013-08-30 23:07:03 +0530

    Get the name of the rep in the picture

--- js/repwho.js
+++ js/repwho.js
@@ -1,16 +1,18 @@
 $(document).ready(
 	$.getJSON('https://reps.mozilla.org/api/v1/rep/?offset=0&limit=0&group=council&callback=?',
 		function (data) {
-			var rep = JSON.stringify(
-				data.objects[Math.floor(
-					Math.random() * data.objects.length
-				)]
-			);
-			rep = JSON.parse(rep);
+			var id = Math.floor(Math.random() * data.objects.length)
+			,	rep = data.objects[id];
+
+			window.data = data;
 
 			$('body').html(
-				"<img src='" + rep.profile.avatar_url + "' />"
+				"<img src='" + rep.profile.avatar_url + "' data-id='"+ id +"' />"
 			);
+
+			$("img").on("click", function (event) {
+				console.log(data.objects[$(this).data("id")].fullname);
+			});
 		},
 		function (error) {
 			console.log(error);
