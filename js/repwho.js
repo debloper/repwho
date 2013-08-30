3d1140b8bcc092e4f7d52c22ac4266db62c1220b
Soumya Deb <debloper@gmail.com>
2013-08-31 03:28:08 +0530

    Awesomeness is landing... check details - Hides mode-selector on click - Uses image-src instead of div-bg for profile image - Updates CSS for the profile image

--- js/repwho.js
+++ js/repwho.js
@@ -6,16 +6,20 @@ $(document).ready(
 
 			window.data = data;
 
-			$('body').html(
-				"<img src='" + rep.profile.avatar_url + "' data-id='"+ id +"' />"
-			);
+			$("#rep_image").attr("data-id", id).attr("src", rep.profile.avatar_url + "&size=256");
+			console.log(rep.profile.avatar_url);
 
-			$("img").on("click", function (event) {
-				console.log(data.objects[$(this).data("id")].fullname);
-			});
+			// $("img").on("click", function (event) {
+			// 	console.log(data.objects[$(this).data("id")].fullname);
+			// });
 		},
 		function (error) {
 			console.log(error);
 		}
 	)
 );
+
+$("#mode_selector").on("click", "button", function () {
+	$("#mode_selector").hide();
+	$("#rep_quiz").show();
+});
