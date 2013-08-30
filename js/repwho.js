de14a06d2a320541e955f62c8172fa988e3bdc3e
Soumya Deb <debloper@gmail.com>
2013-08-30 22:41:10 +0530

    Let's find the profile image of the Rep & append to body

--- js/repwho.js
+++ js/repwho.js
@@ -1,10 +1,15 @@
 $(document).ready(
 	$.getJSON('https://reps.mozilla.org/api/v1/rep/?offset=0&limit=0&group=council&callback=?',
 		function (data) {
-			$('body').append(
-				JSON.stringify(
-					data.objects[Math.floor(Math.random() * data.objects.length)]
-				)
+			var rep = JSON.stringify(
+				data.objects[Math.floor(
+					Math.random() * data.objects.length
+				)]
+			);
+			rep = JSON.parse(rep);
+
+			$('body').html(
+				"<img src='" + rep.profile.avatar_url + "' />"
 			);
 		},
 		function (error) {
