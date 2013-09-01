cd2cd5e4e79d8afc7f995361774dad5ba18ec721
Soumya Deb <debloper@gmail.com>
2013-09-01 20:24:19 +0530

    Caching responses [not on localStorage yet]

--- js/repwho.js
+++ js/repwho.js
@@ -3,27 +3,28 @@
 	window.repwho = {};
 
 	repwho.go = function (mode) {
-		var url = "https://reps.mozilla.org/api/v1/rep/?offset=0&limit=0&callback=?&group=" + mode;
-		$.getJSON( url,
-			function (data) {
-				repwho.data = data;
-				repwho.pick();
-				repwho.pushDOM();
-				$("#rep_names button").shuffle();
-			},
-			function (error) {
-				console.log(error);
-			}
-		)
+		repwho.mode = mode;
+		if (!repwho[mode]) {
+			var url = "https://reps.mozilla.org/api/v1/rep/?offset=0&limit=0&callback=?&group=" + mode;
+			$.getJSON( url,
+				function (data) {
+					repwho[mode] = data;
+					repwho.repeat();
+				},
+				function (error) {
+					console.log(error);
+				}
+			)
+		} else repwho.repeat();
 	};
 
 	repwho.pick = function () {
-		repwho.id = Math.floor(Math.random() * repwho.data.objects.length);
-		repwho.rep = repwho.data.objects[repwho.id];
+		repwho.id = Math.floor(Math.random() * repwho[repwho.mode].objects.length);
+		repwho.rep = repwho[repwho.mode].objects[repwho.id];
 	};
 
 	repwho.random = function () {
-		repwho.tmp = Math.floor(Math.random() * repwho.data.objects.length);
+		repwho.tmp = Math.floor(Math.random() * repwho[repwho.mode].objects.length);
 		if (repwho.tmp == repwho.id) repwho.random();
 		return repwho.tmp;
 	};
@@ -32,20 +33,25 @@
 		$("#rep_image").attr("data-id", repwho.id).attr("src", repwho.rep.profile.avatar_url + "&size=256");
 		$("#rep_names").html(
 			"<button id='richtig'>"+ repwho.rep.fullname +"</button>" +
-			"<button class='falsch'>"+ repwho.data.objects[repwho.random()].fullname +"</button>" +
-			"<button class='falsch'>"+ repwho.data.objects[repwho.random()].fullname +"</button>" +
-			"<button class='falsch'>"+ repwho.data.objects[repwho.random()].fullname +"</button>"
+			"<button class='falsch'>"+ repwho[repwho.mode].objects[repwho.random()].fullname +"</button>" +
+			"<button class='falsch'>"+ repwho[repwho.mode].objects[repwho.random()].fullname +"</button>" +
+			"<button class='falsch'>"+ repwho[repwho.mode].objects[repwho.random()].fullname +"</button>"
 		);
 	};
 
 	repwho.reset = function () {
-		$("#mode_selector").show();
-		$("#rep_quiz").hide();
-		$("#quiz_result").hide();
-		$("#rep_image").removeAttr("src");
-		$("#rep_names").html("");
+		repwho.repeat();
+		$("#rep_quiz").show();
 		$("#baDumTss").html("");
+		$("#quiz_result").hide();
+		$("#rep_image").attr("src", "");
 	};
+
+	repwho.repeat = function () {
+		repwho.pick();
+		repwho.pushDOM();
+		$("#rep_names button").shuffle();
+	}
 
 	$("#mode_selector").on("click", "button", function () {
 		window.mode = $(this).data("mode");
